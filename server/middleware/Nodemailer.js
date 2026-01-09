aeb0eb1f1683a82b9f169de131e690630565a2d0
Ashvary1996 <ashvarygidian1996@gmail.com>
2026-01-09 15:31:19 +0530

    packages updated

--- server/middleware/Nodemailer.js
+++ server/middleware/Nodemailer.js
@@ -2,13 +2,22 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, htmlContent) => {
   try {
+    // const transporter = nodemailer.createTransport({
+    //   service: process.env.SERVICE,
+    //   host: "smtp.gmail.com" , // e.g//process.env.SMTP_HOST 'smtp.gmail.com'
+    //   port: 587,
+    //   secure: (process.env.SMTP_PORT || 587) == 465, // true for 465, false for other ports
+    //   pool: true, // Use a connection pool
+    //   timeout: 10000, // 10 seconds
+    //   auth: {
+    //     user: process.env.EMAIL,
+    //     pass: process.env.PASSWORD,
+    //   },
+    // });
     const transporter = nodemailer.createTransport({
-      service: process.env.SERVICE,
-      host: "smtp.gmail.com" , // e.g//process.env.SMTP_HOST 'smtp.gmail.com'
+      host: "smtp.gmail.com", // e.g//process.env.SMTP_HOST 'smtp.gmail.com'
       port: 587,
-      secure: (process.env.SMTP_PORT || 587) == 465, // true for 465, false for other ports
-      pool: true, // Use a connection pool
-      timeout: 10000, // 10 seconds
+      secure: false, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,
